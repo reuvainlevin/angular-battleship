b18ea25e49979caae3c85e35aab7a6083b13f556
reuvainlevin@gmail.com <reuvainlevin@gmail.com>
2019-09-15 19:11:28 -0400

    place boat func

---
+++ src/app/modules/placeBoat.ts
@@ -0,0 +1,66 @@
+import { getRandomNumber } from './getRandomNumber';
+import { IBoard } from '../interfaces/board';
+import { IBoatPart } from '../interfaces/boat-part';
+
+export function placeBoat( board: IBoard, boatSize: number, boatId: number, boardWidth: number, boardHeight: number ): IBoard {
+    boardWidth = boardWidth - 1;  // because of 0 based arrays
+    boardHeight = boardHeight - 1;
+    let placed = false;
+    // let fullBoatLoc = [];
+    while ( !placed ) {
+        const tmpBoatParts: { row: number, col: number, boatPart: IBoatPart}[] = [];
+        let randomRow = getRandomNumber( 0, boardWidth );
+        let randomCol = getRandomNumber( 0, boardHeight );
+        const randomDir = getRandomNumber( 1, 4 );
+        const boatDir = ( randomDir === 1 || randomDir === 2 ) ? 'd' : 'a';
+        let boatSpot = ( randomDir === 1 || randomDir === 3 ) ? boatSize : 1;
+
+        for ( let i = 0; i < boatSize; i++ ) {
+            if ( i > 0 ) {
+                switch ( randomDir ) {
+                    case 1: randomRow--;
+                            boatSpot--;
+                            break;
+                    case 2: randomRow++;
+                            boatSpot++;
+                            break;
+                    case 3: randomCol--;
+                            boatSpot--;
+                            break;
+                    case 4: randomCol++;
+                            boatSpot++;
+                }
+            }
+            if (
+                randomRow < 0 ||
+                randomRow > boardWidth ||
+                randomCol < 0 ||
+                randomCol > boardHeight ||
+                board.squares.find( sq => sq.row === randomRow && sq.col === randomCol ).boatPart
+            ) {
+                break;
+            } else {
+                tmpBoatParts.push( {
+                    row: randomRow,
+                    col: randomCol,
+                    boatPart: {
+                        boatId,
+                        boatSize,
+                        boatSpot,
+                        boatImagePath: ( '' + boatDir + boatSpot + boatSize ),
+                        isHit: false
+                    }
+                } );
+            }
+        }
+        if ( tmpBoatParts.length === boatSize ) {
+            tmpBoatParts.forEach( part => {
+                board.squares.find( sq => sq.row === part.row && sq.col === part.col ).boatPart = part.boatPart;
+            } );
+            placed = true;
+            // fullBoatLoc = tmpBoatParts;
+        }
+    }
+
+    return board;
+}
