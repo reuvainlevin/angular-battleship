1dc71c30fb81b95109a12c3c5af9d87c8f96ea60
reuvainlevin@gmail.com <reuvainlevin@gmail.com>
2019-09-15 22:29:56 -0400

    small updates too testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import {startGame, pauseGame, endGame, continueGame} from './store/actions/game-state.actions';
-import { getGameStarted$, getGamePaused$, getGameEnded$ } from './store/selectors/feature-game-state.selectors';
+import {startGame, pauseGame, endGame, continueGame} from './store/actions/feature-game-state.actions';
+import { getGameStarted$, getGamePaused$, getGameEnded$ } from './store/selectors/feature-game.selectors';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import { AppState } from './store/interfaces/app-state';
+import { getBoard } from './modules/getBoard';
+import { placeBoat } from './modules/placeBoat';
+import { getFeatureBoard$ } from './store/selectors/feature-board.selectors';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +19,13 @@ export class AppComponent implements OnInit {
   isStarted$: Observable<boolean>;
   isPaused$: Observable<boolean>;
   isEnded$: Observable<boolean>;
+  boatArray = [ 2, 3, 3, 4, 5 ];
+  boatId = 1;
+  boardSize = { // move to store
+    width: 7,
+    height: 7
+  };
+  board = getBoard( this.boardSize.width, this.boardSize.height );
 
   constructor(private store: Store<AppState> ) { }
 
@@ -23,6 +33,16 @@ export class AppComponent implements OnInit {
     this.isStarted$ = this.store.pipe( select( getGameStarted$ ) );
     this.isPaused$ = this.store.pipe( select( getGamePaused$ ) );
     this.isEnded$ = this.store.pipe( select( getGameEnded$ ) );
+
+    // this.boatArray.forEach( boat => {
+    //   placeBoat( this.board, boat, this.boatId++, this.boardSize.width, this.boardSize.height );
+    // });
+    // console.log( this.board );
+
+    this.store.pipe( select( getFeatureBoard$ ) ).subscribe( b => {
+      console.log( 'this is my board from store: ', b );
+    } );
+
   }
 
   startGame(): void {
