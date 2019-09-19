d17384d539db05483639dfa6e2b144e9fb852f70
reuvainlevin@gmail.com <reuvainlevin@gmail.com>
2019-09-18 21:32:09 -0400

    moved getBoard function to board model

--- src/app/models/board.ts
+++ src/app/models/board.ts
@@ -1,8 +1,39 @@
 import { IFeatureBoard } from '../store/interfaces/feature-board';
 import { ISquare } from '../store/interfaces/square';
+import { IFeatureBoardSpecs } from '../store/interfaces/feature-board-specs';
+import { placeBoat } from '../modules/placeBoat';
 export class Board implements IFeatureBoard {
-    squares: ISquare[];
-    constructor() {
-        this.squares = [];
+    squares = {};
+    constructor( private boardSpecs: IFeatureBoardSpecs ) {
+        this.squares = this.getBoard( this.boardSpecs.rows, this.boardSpecs.cols );
+        this.fillBoard( this, this.boardSpecs );
     }
+
+    private getBoard( rows: number, cols: number ): {[id: string]: ISquare} {
+        const squares = {};
+        let id = 1;
+
+        for ( let row = 0; row < rows; row++ ) {
+            for ( let col = 0; col < cols; col++ ) {
+                squares[row.toString() + col.toString()] = {
+                    id: id++,
+                    row,
+                    col
+                };
+            }
+        }
+
+        return squares;
+    }
+
+    private fillBoard( board: IFeatureBoard, boardSpecs: IFeatureBoardSpecs ): void {
+        boardSpecs.boats.forEach( boat => {
+            placeBoat( board, boat, boardSpecs.rows, boardSpecs.cols );
+        } );
+    }
+
+    // private adddBoat( board: IFeatureBoard, boardSpecs:  ): void {
+
+    // }
+
 }
--- src/app/modules/getBoard.ts
+++ src/app/modules/getBoard.ts
@@ -1,19 +1,19 @@
-import { Board } from './../models/board';
-import { IFeatureBoard } from '../store/interfaces/feature-board';
+// import { Board } from './../models/board';
+// import { IFeatureBoard } from '../store/interfaces/feature-board';
 
-export function getBoard( rows: number, cols: number): IFeatureBoard {
-    const board = new Board();
-    let id = 1;
+// export function getBoard( rows: number, cols: number): IFeatureBoard {
+//     const board = new Board();
+//     let id = 1;
 
-    for ( let row = 0; row < rows; row++) {
-        for ( let col = 0; col < cols; col++ ) {
-            board.squares.push( {
-                id: id++,
-                row,
-                col
-            } );
-        }
-    }
+//     for ( let row = 0; row < rows; row++) {
+//         for ( let col = 0; col < cols; col++ ) {
+//             board.squares.push( {
+//                 id: id++,
+//                 row,
+//                 col
+//             } );
+//         }
+//     }
 
-    return board;
-}
+//     return board;
+// }
